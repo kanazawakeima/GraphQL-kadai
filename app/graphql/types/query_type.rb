b65e645ae14c5dfae3c3125a674751aa52c23a9c
kanazawa keima <little-green7730@softbank.ne.jp>
2022-05-23 02:18:49 +0900

    課題提出

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,16 @@
+module Types
+  class QueryType < Types::BaseObject
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    field :test_field, String, null: false,
+      description: "An example field added by the generator"
+    def test_field
+      "Hello World!"
+    end
+    field :blog, BlogType, null: false
+    def blog
+      context[:blog]
+    end
+  end
+end
